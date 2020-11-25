b3faa8277f58915ed95d3c35a2d7677fcf3df343
dmensah1 <dmensah4@uwo.ca>
2020-11-24 19:25:12 -0500

    Project initialized

---
+++ test.js
@@ -0,0 +1,8 @@
+var assert = require('assert');
+//var fn = require('./purchaseOrder');
+
+describe('Structural Testing', function() {
+    it('teest', function() {
+        assert.equal(5, 5);
+    })
+});
